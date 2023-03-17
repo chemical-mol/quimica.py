e4560eaafa36666a3eb77f314c05686872f770fd
Alisson <126930556+chemical-mol@users.noreply.github.com>
2023-03-17 08:33:36 -0300

    atualização 4
    nem sei mais o que coloquei aqui

--- quimica.py
+++ quimica.py
@@ -2,6 +2,7 @@ from molmass import Formula
 from molmass import ELEMENTS, Element
 from tkinter import *
 from tkinter import ttk
+from mendeleev import element
 
 
 class chemical:
@@ -17,7 +18,7 @@ class chemical:
       self.ap_mol.config(text=f'{self.my_var} g/mol')
       self.ap_comp.config(text=f'{self.comp} ')
       self.ap_atom.config(text=f'existe(m) {self.atom} átomo(s)')
-      print(self.my_var)
+
     except Exception:
       self.ap_mol.config(text=f'ALGO FOI DIGITADO INCORRETAMENTE')
       self.ap_comp.config(text=f'VERIFIQUE O QUE FOI ESCRITO ')
@@ -25,28 +26,35 @@ class chemical:
         text=f'SOMETHING WAS ENTERED INCORRECTLY,\n CHECK WHAT WAS WRITTEN')
 
   #________________________Elementos_______________________________
-
-  def elemento(self):
+ #FRAME TELA1
+  def elemento(self): #FRAME TELA2
     try:
       self.t_elem = self.caixa_elem.get()
-      self.p_elem = ELEMENTS[self.t_elem]
+      self.p_elem = ELEMENTS[self.t_elem] #MOLMASS
+      self.p_elem_mendeleev = element(self.t_elem) #MENDELEEV 
       self.nome_e = self.p_elem.name
       self.simbolo = self.p_elem.symbol
       self.conf = self.p_elem.eleconfig
-      self.e_conf = self.p_elem.eleconfig_dict
+      self.e_conf = self.p_elem_mendeleev.oxistates #mendeleev 
+      self.atomic = self.p_elem_mendeleev.atomic_weight #mendeleev
       self.ap_elem.config(text=f'{self.nome_e}')
       self.ap_simb.config(text=f'{self.simbolo}')
       self.ap_conf.config(text=f'{self.conf}')
       self.ap_confe.config(text=f'{self.e_conf}')
+      self.ap_atomic.config(text=f'{self.atomic}')
     except:
       self.ap_elem.config(text=f'ALGO FOI DIGITADO INCORRETAMENTE')
       self.ap_simb.config(text=f'VERIFIQUE O QUE FOI ESCRITO ')
       self.ap_conf.config(
         text=f'SOMETHING WAS ENTERED INCORRECTLY,\n CHECK WHAT WAS WRITTEN')
       self.ap_confe.config(text=f'Erro!')
+      self.ap_atomic.config(text=f'Erro!')
 
   #___ ______________________________________________________________
 
+
+  def mendleev():
+    pass 
   def __init__(self, master):
 
     self.tela_entrar = Tk()
@@ -56,18 +64,24 @@ class chemical:
     self.note = ttk.Notebook(self.tela_entrar)
     self.note.place(x=5, y=0, width=290, height=440)
 
-    #frame MOLECULA
+    #frame COMPOSTO
 
     self.tela1 = Frame(self.tela_entrar,
                        bg='#D8E1FF',
                        borderwidth=2,
                        relief='sunken')
-    self.note.add(self.tela1, text='MOLÉCULA')
+    self.note.add(self.tela1, text='COMPOSTO')
     self.tela2 = Frame(self.tela_entrar,
                        bg='#D8E1FF',
                        borderwidth=2,
                        relief='sunken')
     self.note.add(self.tela2, text='ELEMENTO')
+
+    #self.tela3 = Frame(self.tela_entrar,
+   #                    bg='#D8E1FF',
+   #                    borderwidth=2,
+   #                    relief='sunken')
+    #self.note.add(self.tela3, text='TABELA')
     #____________inicio___________________
 
     #caixas entry
@@ -79,12 +93,9 @@ class chemical:
                            )  #caixa de massa molecular e composição
     self.caixa_mol.place(x=40, y=25, width=200)
 
-    #self.caixa_elem = Entry(self.tela1, )  #caixa para Elementos
-    #self.caixa_elem.place(x=40, y=200, width=200)
-
     #________________________________
     #ligação com def
-    self.my_var = StringVar()
+
     self.mas_comp = ""
 
     self.element = ""
@@ -122,13 +133,27 @@ class chemical:
     self.ap_conf.place(x=20, y=180)
 
     self.ap_confe = Label(self.tela2,
-                          text='ELETRO CONFIGURAÇÃO ',
+                          text='ESTADO DE OXIDAÇÃO',
                           bg='#D8E1EE')
     self.ap_confe.place(x=20, y=240)
 
+    self.ap_atomic = Label(self.tela2,
+                          text='PESO ATÔMICO',
+                          bg='#D8E1EE')
+    self.ap_atomic.place(x=20, y=300)
+
     self.botao = Button(self.tela2, text="Processar", command=self.elemento)
     self.botao.place(x=60, y=350)
+     #____________________________________
+     #______________FRAME3______________________
+    
 
+
+
+
+
+
+    
     #____________________________________
     self.tela_entrar.mainloop()
 
